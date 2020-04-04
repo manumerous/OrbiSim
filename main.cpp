4c35156a60fdeb5a13122a608cc30ba090b59fc8
Manumerous <manuel.galliker@gmx.ch>
2020-04-04 23:30:09 +0200

    added switch case statement and text event

--- main.cpp
+++ main.cpp
@@ -1,21 +1,37 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "Orbital Simulator!");
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "OrbiSim: Orbital Simulator!");
+    
     sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Red);
+    shape.setFillColor(sf::Color::Black);
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        sf::Event evnt;
+        while (window.pollEvent(evnt))
         {
-            if (event.type == sf::Event::Closed)
+            switch(evnt.type)
+            {
+            case sf::Event::Closed:
                 window.close();
+                break;
+            case sf::Event::Resized:
+                // std::cout << "New Window Size: " << evnt.size.width << ", " << evnt.size.height << std::endl;
+                printf("New window size: %i, %i\n", evnt.size.width, evnt.size.height);
+                break;
+            case sf::Event::TextEntered:
+                if(evnt.text.unicode < 128)
+                {
+                    printf("%c", evnt.text.unicode);
+                }
+                    
+            }
         }
 
-        window.clear();
+        window.clear(sf::Color::White);
         window.draw(shape);
         window.display();
     }
